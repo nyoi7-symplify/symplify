9fad068df6d446d46d8520d57ba09fd644d7ea98
ChrisJettoo <cjettoostudent@gmail.com>
2023-11-06 18:39:08 -0500

    added some middleware and sql model

---
+++ server/controller.js
@@ -0,0 +1,39 @@
+const db = require('./model.js');
+const { spawn } = require('child_process');
+
+
+const runtimeController = {};
+
+
+runtimeController.scriptBuilder = (req, res, next) => {
+
+
+
+
+}
+
+runtimeController.scriptRunner = (req, res, next) => {
+
+
+
+  
+}
+
+runtimeController.storeRuns = (req, res, next) => {
+
+
+
+  
+}
+
+
+runtimeController.returnRun = (req, res, next) => {
+
+
+
+  
+}
+
+
+
+module.exports = runtimeController;
