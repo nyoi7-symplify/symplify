62773193dadcd497ceb9f9b57c6afe0bb9be3b96
ChrisJettoo <cjettoostudent@gmail.com>
2023-11-07 17:14:12 -0500

    created resultsbox component

--- server/scripts/test.js
+++ server/scripts/test.js
@@ -1,1 +1,22 @@
-console.log('hello world!');
+function fibonacci(n) {
+  if (n <= 0) {
+    return [];
+  } else if (n === 1) {
+    return [0];
+  } else if (n === 2) {
+    return [0, 1];
+  }
+
+  const sequence = [0, 1];
+  for (let i = 2; i < n; i++) {
+    const nextValue = sequence[i - 1] + sequence[i - 2];
+    sequence.push(nextValue);
+  }
+
+  return sequence;
+}
+
+// Example usage:
+const n = 10; // Calculate the first 10 Fibonacci numbers
+const result = fibonacci(n);
+console.log(result); // Output: [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
--- src/app.jsx
+++ src/app.jsx
@@ -4,14 +4,19 @@ import CodeMirror from '@uiw/react-codemirror';
 // import 'codemirror/theme/dracula.css';
 import { javascript } from '@codemirror/lang-javascript';
 import { darcula } from '@uiw/codemirror-theme-darcula';
+import ResultsBox from './results.jsx';
+
 
 const App = () => {
   const [value, setValue] = React.useState("console.log('hello world!');");
-  
+  const [results, setResults] = useState(null); 
+ 
   const onChange = React.useCallback((val, viewUpdate) => {
-    // console.log('val:', val);
+    console.log('val:', val);
     setValue(val);
   }, []);
+  
+
 
   async function handleRun() {    
     const res = await fetch('/api/algoCode',
@@ -23,9 +28,14 @@ const App = () => {
       },
       body: value
     })
-    console.log(await res.json())
-  }
+    const data = await res.json();
+    setResults(data);
+    // console.log(await res.json())
   
+  }
+
+
+  console.log('this is results', results);
   return (
     <div className="App">
       <CodeMirror 
@@ -36,6 +46,9 @@ const App = () => {
       extensions={[javascript({ jsx: true })]}/>
 
       <button onClick={handleRun}>Run</button>
+
+      {results !== null ? <ResultsBox data={results} /> : null}
+     
     </div>
   )
 }
