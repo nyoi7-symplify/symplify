1635d3b8bbfcabf8edc671304c3a1d7973cec0bc
ChrisJettoo <cjettoostudent@gmail.com>
2023-11-08 14:58:28 -0500

    made some changes to css

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+// Navbar.js
+
+import React from 'react';
+import logo from '/src/files/logo.png'
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+     <img className='logo' src={logo}></img>
+
+      </div>
+      <ul className="navbar-links">
+        <li className="navbar-link">
+          <a href="#">Home</a>
+        </li>
+        <li className="navbar-link">
+          <a href="#">About</a>
+        </li>
+        <li className="navbar-link">
+          <a href="#">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
